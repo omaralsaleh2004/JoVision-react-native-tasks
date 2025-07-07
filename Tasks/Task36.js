5fb0d97b84a2e41ed27ec865fbe4a9fce99bbbc6
Your Name <you@example.com>
2025-07-07 12:37:15 +0300

    Completed Task 37

--- Tasks/Task36.js
+++ Tasks/Task36.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, Text, View, StyleSheet } from 'react-native';
 
 function generateRandomWord(length) {
   let result = '';
@@ -11,16 +11,25 @@ function generateRandomWord(length) {
 }
 
 const Task36 = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  };
+
   const words = [];
   for (let i = 0; i < 100; i++) {
     words.push(generateRandomWord(5 + Math.floor(Math.random() * 6))); //from 5 to 10 characters
   }
 
   return (
-    <View>
-      <ScrollView>
+    <View style={styles.container}>
+      <ScrollView refreshing={refreshing} onRefresh={onRefresh}>
         {words.map((word, index) => (
-          <Text key={index}>
+          <Text style={styles.text} key={index}>
             {index + 1}. {word}
           </Text>
         ))}
@@ -30,3 +39,13 @@ const Task36 = () => {
 };
 
 export default Task36;
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    paddingHorizontal: 20,
+  },
+  text: {
+    fontSize: 18,
+    paddingVertical: 4,
+  },
+});
